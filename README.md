eae3a9a05210737dfc4d6183d5c307f7f7702053
Duke <duke@fungies.io>
2026-04-09 14:37:13 +0200

    Initial commit: WordPress plugin scaffold

---
+++ README.md
@@ -0,0 +1,22 @@
+# Fungies WP Plugin
+
+A WordPress plugin for digital selling powered by [Fungies](https://fungies.io).
+
+## Description
+
+This plugin integrates Fungies digital selling capabilities directly into your WordPress site.
+
+## Installation
+
+1. Download the plugin zip file
+2. Go to WordPress Admin > Plugins > Add New > Upload Plugin
+3. Upload the zip file and activate
+
+## Requirements
+
+- WordPress 5.8 or higher
+- PHP 7.4 or higher
+
+## License
+
+GPLv2 or later
---
+++ fungies-wp-plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Fungies WP Plugin
+ * Plugin URI: https://github.com/dukenukemall/fungies-wp-plugin
+ * Description: WordPress plugin for digital selling powered by Fungies.
+ * Version: 1.0.0
+ * Author: Fungies
+ * Author URI: https://fungies.io
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: fungies-wp-plugin
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+define( 'FUNGIES_WP_VERSION', '1.0.0' );
+define( 'FUNGIES_WP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'FUNGIES_WP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
